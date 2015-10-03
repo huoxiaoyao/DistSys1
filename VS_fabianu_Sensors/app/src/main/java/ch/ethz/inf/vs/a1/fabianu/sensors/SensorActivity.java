7947075142469accbf182026931c9dd52f7668b3
huoxiaoyao <fabianu@student.ethz.ch>
2015-10-03 11:02:15 +0200

    changed more names, layout to fit specs

--- VS_fabianu_Sensors/app/src/main/java/ch/ethz/inf/vs/a1/fabianu/sensors/SensorActivity.java
+++ VS_fabianu_Sensors/app/src/main/java/ch/ethz/inf/vs/a1/fabianu/sensors/SensorActivity.java
@@ -1,24 +1,25 @@
 package ch.ethz.inf.vs.a1.fabianu.sensors;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import ch.ethz.inf.vs.a1.fabianu.sensors.R;
-
-public class SensorActivity2 extends AppCompatActivity {
+public class SensorActivity extends ListActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensor2);
+        setContentView(R.layout.activity_sensor);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sensor_activity2, menu);
+        getMenuInflater().inflate(R.menu.menu_sensor_activity, menu);
         return true;
     }
 
@@ -35,5 +36,15 @@ public class SensorActivity2 extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
